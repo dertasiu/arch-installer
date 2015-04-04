9d9f7eb5b9c45d21ea21af8b8fbe0dd3a2a9379d
Mario Gordillo Ortiz <mariogordilloortiz@gmail.com>
2015-04-04 16:07:19 +0200

    Comentado y marcado como TODO la seleccion de la zona horaria, añadido lo siguiente: grub, ususurio, contraseña del usuario, habilitado sudo en usuario, habilitado y actualizado aur (reverted from commit 07baef69dcf6d57a4e443c531649a4e4aafe7938)

--- Install.sh
+++ Install.sh
@@ -386,11 +386,9 @@ timezone="$(cat temp)"
 rm temp
 if [ "$?" = "0" ]
 then
-	while [ $eleccion = "0" ]
-	do
-		if test -f $timezone; then
-			ln -s $timezone timezone >&2
-		else
-			cd $timezone >&2
-		fi
+	if test -f $timezone; then
+		ln -s $timezone timezone >&2
+	else
+	echo "Yuck! Where is it??" >&2
+	fi
 fi
