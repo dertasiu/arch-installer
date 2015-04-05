98550d80b7365199b97a2a6c02cdfa1e809c3267
Mario Gordillo Ortiz <mariogordilloortiz@gmail.com>
2015-04-05 19:08:24 +0200

    Corregida la creacion de usuarios

--- Install.sh
+++ Install.sh
@@ -441,11 +441,11 @@ dialog --backtitle "Archlinux Installation" --title "User creation" \
 		"Username :" 1 1 "user" 1 25 25 30 \
 		"Real name:" 2 1 "Human Foo Bar" 2 25 25 30 2>temp
 user=$(cat temp | sed -n 1p)
-realname=$(cat temp | sed -n 2p)
+realname=$(cat temp | sed -n 2p | sed 's/^/"/' | sed 's/$/"/')
 rm temp
 if [ "$?" = "0" ]
 then
-	arch-chroot /mnt /bin/bash -c "useradd -c "$realname" -m -g users -G video,audio,lp,optical,games,power,wheel,storage -s /bin/bash $user"
+	arch-chroot /mnt /bin/sh -c "useradd -c $realname -m -g users -G video,audio,lp,optical,games,power,wheel,storage -s /bin/bash $user"
 	clear
 	echo "Please, enter the password that the user will use:"
 	arch-chroot /mnt /bin/bash -c "passwd $user"
