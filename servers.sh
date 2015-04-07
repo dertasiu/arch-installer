311b5ed1f7c5bef13410deb408b4198e79b38019
Mario Gordillo Ortiz <mariogordilloortiz@gmail.com>
2015-04-07 19:17:56 +0200

    Se elimina el bucle para repetir, se sustituye por algo tipo: if web esta instalado continua, else, instala el servidor web. Se cambian los comandos de instalación de pacman, la instalación de los servidores se realizará en el sistema reiniciado y no en el Live CD

--- servers.sh
+++ servers.sh
@@ -1,78 +1,84 @@
-if [[ repeat = "0" ]]
-	cmd=(dialog --backtitle "ArchLinux Installation" --separate-output --checklist "Select the Services that you want to install:" 22 76 16)
-	options=(SSH "Remote console"	off
-			Web "Apache + PHP5 + MariaDB(Mysql) A complete Web Server"	off
-			Owncloud "Self-hosted cloud"	off
-			Wordpress "Self-hosted blog"	off
-			Subsonic "Music Server"	off
-			NTOP "Traffic monitoring tool"	off
-			TightVNC "Remote screen server"	off
-			Deluge "Torrent server with web UI"	off
-			PPTP "VirtualPrivateNetwork Server"	off
-			Prosody "XMPP Chat Server"	off
-			)
-	desktop=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-	clear
-	for choice in $desktop
-	do
-		case $choice in
-			"SSH")
-				SSH_INSTALLED=1
-				arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm openssh"
-				dialog --backtitle "ArchLinux Installation" --title "SSH Configuration" \
-						--yesno "Do you want to change the default port(22) of SSHD?" 7 60 
-				response=$?
-				case $response in
-					0) dialog --backtitle "Archlinux Installation" --title "SSH Configuration" \
-								--inputbox "Enter the port that you want to use:" 8 40 2>temp
-						port=$(cat temp)
-						rm temp
-						if [ "$?" = "0" ]
-						then
-							sed -i -e "s/#Port 22/Port $(echo $port)/g" /mnt/etc/ssh/sshd_config
-						fi;;
-					1) echo "Port not changed";;
-				esac
-			;;
+LAMP=0
+cmd=(dialog --backtitle "ArchLinux Installation" --separate-output --checklist "Select the Services that you want to install:" 22 76 16)
+options=(SSH "Remote console"	off
+		Web "Apache + PHP5 + MariaDB(Mysql) A complete Web Server"	off
+		Owncloud "Self-hosted cloud"	off
+		Wordpress "Self-hosted blog"	off
+		Subsonic "Music Server"	off
+		NTOP "Traffic monitoring tool"	off
+		TightVNC "Remote screen server"	off
+		Deluge "Torrent server with web UI"	off
+		PPTP "VirtualPrivateNetwork Server"	off
+		Prosody "XMPP Chat Server"	off
+		)
+desktop=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+clear
+for choice in $desktop
+do
+	case $choice in
+		"SSH")
+			pacman -Syy --noconfirm openssh
+			dialog --backtitle "ArchLinux Installation" --title "SSH Configuration" \
+					--yesno "Do you want to change the default port(22) of SSHD?" 7 60 
+			response=$?
+			case $response in
+				0) dialog --backtitle "Archlinux Installation" --title "SSH Configuration" \
+							--inputbox "Enter the port that you want to use:" 8 40 2>temp
+					port=$(cat temp)
+					rm temp
+					if [ "$?" = "0" ]
+					then
+						sed -i -e "s/#Port 22/Port $(echo $port)/g" /etc/ssh/sshd_config
+					fi;;
+				1) echo "Port not changed";;
+			esac
+		;;
 
-			"Web")
-				arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm gnome gnome-extra"
-			;;
+		"Web")
+			pacman -Syy --noconfirm apache php php-apache mariadb
 
-			"Owncloud")
-				arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm mate mate-extra"
-			;;
+			mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+			'/usr/bin/mysqladmin' -u root password 'mysql'
+			sed -i '/%wheel ALL=(ALL) ALL/s/^#//g' /etc/sudoers
+			LAMP=1
+		;;
 
-			"Wordpress")
-				arch-root /mnt /bin/bash -c "yaourt -Syy --noconfirm pantheon-session-bzr"
-			;;
+		"Owncloud")
+			if [[ $LAMP == "0" ]]; then
+				web
+			fi
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm owncloud"
+		;;
 
-			"Subsonic")
-				arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm xfce4"
-			;;
+		"Wordpress")
+			if [[ $LAMP == "0" ]]; then
+				web
+			fi
+			arch-chroot /mnt /bin/bash -c "yaourt -Syy --noconfirm wordpress"
+		;;
 
-			"NTOP")
-				arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm lxde-common"
-			;;
+		"Subsonic")
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm "
+		;;
 
-			"TightVNC")
-				arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm lxqt"
-			;;
+		"NTOP")
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm "
+		;;
 
-			"Deluge")
-				printf "[Unity-for-Arch]\nSigLevel = Optional TrustAll\nServer = http://dl.dropbox.com/u/486665/Repos/$repo/$arch\[Unity-for-Arch-Extra]\nSigLevel = Optional TrustAll\nServer = http://dl.dropbox.com/u/486665/Repos/$repo/$arch" >> /mnt/etc/pacman.conf
-				arch-root /mnt /bin/bash -c "yaourt -Syy --noconfirm unity"
-			;;
+		"TightVNC")
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm "
+		;;
 
-			"PPTP")
-				arch=$(uname -m)
-				printf "[home_metakcahura_arch-deepin_Arch_Extra]\nSigLevel = Never\nServer = http://download.opensuse.org/repositories/home:/metakcahura:/arch-deepin/Arch_Extra/$arch" >> /mnt/etc/pacman.conf
-				arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm deepin deepin-extra"
-			;;
+		"Deluge")
+			arch-chroot /mnt /bin/bash -c "yaourt -Syy --noconfirm "
+		;;
 
-			"Prosody")
-				arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm openbox"
-			;;
-	esac
-	done
-fi
+		"PPTP")
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm "
+		;;
+
+		"Prosody")
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm "
+		;;
+esac
+done
