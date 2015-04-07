2f1023484a3e23fe76cdabc3ce2f602bad17716d
Sebas <bastiqui@gmail.com>
2015-04-07 16:29:34 +0000

    Corregido el "arch-root" por "arch-chroot" y el nombre de Budgie que estaba mal escrito.

--- DesktopEnvironment.sh
+++ DesktopEnvironment.sh
@@ -12,7 +12,7 @@ options=(KDE4 " "	off
 		OpenBox " "	off
 		i3 " "	off
 		Cinnamon " "	off
-		Budige " "	off
+		Budgie " "	off
 		)
 desktop=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 clear
@@ -20,62 +20,62 @@ for choice in $desktop
 do
 	case $choice in
 		"KDE4")
-			arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm kde"
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm kde"
 		;;
 
 		"KDE5")
-			arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm plasma"
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm plasma"
 		;;
 
 		"Gnome")
-			arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm gnome gnome-extra"
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm gnome gnome-extra"
 		;;
 
 		"MATE")
-			arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm mate mate-extra"
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm mate mate-extra"
 		;;
 
 		"Pantheon")
-			arch-root /mnt /bin/bash -c "yaourt -Syy --noconfirm pantheon-session-bzr"
+			arch-chroot /mnt /bin/bash -c "yaourt -Syy --noconfirm pantheon-session-bzr"
 		;;
 
 		"XFCE")
-			arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm xfce4"
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm xfce4"
 		;;
 
 		"LXDE")
-			arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm lxde-common"
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm lxde-common"
 		;;
 
 		"LXQT")
-			arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm lxqt"
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm lxqt"
 		;;
 
 		"Unity")
 			printf "[Unity-for-Arch]\nSigLevel = Optional TrustAll\nServer = http://dl.dropbox.com/u/486665/Repos/$repo/$arch\[Unity-for-Arch-Extra]\nSigLevel = Optional TrustAll\nServer = http://dl.dropbox.com/u/486665/Repos/$repo/$arch" >> /mnt/etc/pacman.conf
-			arch-root /mnt /bin/bash -c "yaourt -Syy --noconfirm unity"
+			arch-chroot /mnt /bin/bash -c "yaourt -Syy --noconfirm unity"
 		;;
 
 		"DDE")
 			arch=$(uname -m)
 			printf "[home_metakcahura_arch-deepin_Arch_Extra]\nSigLevel = Never\nServer = http://download.opensuse.org/repositories/home:/metakcahura:/arch-deepin/Arch_Extra/$arch" >> /mnt/etc/pacman.conf
-			arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm deepin deepin-extra"
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm deepin deepin-extra"
 		;;
 
 		"OpenBox")
-			arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm openbox"
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm openbox"
 		;;
 
 		"i3")
-			arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm i3"
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm i3"
 		;;
 
 		"Cinnamon")
-			arch-root /mnt /bin/bash -c "pacman -Syy --noconfirm cinnamon"
+			arch-chroot /mnt /bin/bash -c "pacman -Syy --noconfirm cinnamon"
 		;;
 
 		"Budgie")
-			arch-root /mnt /bin/bash -c "yaourt -Syy --noconfirm  budgie-desktop-git"
+			arch-chroot /mnt /bin/bash -c "yaourt -Syy --noconfirm  budgie-desktop-git"
 		;;
 esac
 done
