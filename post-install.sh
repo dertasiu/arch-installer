9957634b37daa2a74dd21588d9281a65572b5f73
Mario Gordillo Ortiz <mariogordilloortiz@gmail.com>
2015-05-27 10:09:16 +0200

    Corregidas erratas

--- post-install.sh
+++ post-install.sh
@@ -10,15 +10,15 @@ if [[ $? == 0 ]];then
 		      └─Pantheon\n
 		\nKDE4 ── KDE5      ' 0 0
 		cmd=(dialog --backtitle "ArchLinux Installation" --separate-output --checklist "Select the Desktop Environment:" 0 0 0)
-		options=(KDE4 "KDE desktop environment v4 (Conflict with KDE5)"	off
-				KDE5 "KDE desktop environment v5 (Conflict with KDE4)"	off
+		options=(KDE4 "KDE desktop environment v4"	off
+				KDE5 "KDE desktop environment v5"	off
 				Gnome "GNOME Desktop environment"	off
 				XFCE "XFCE desktop environment"	off
 				LXDE "Light Desktop environment"	off
 				MATE "A mantained fork of GNOME v2"	off
 				Pantheon "Elementary OS' Desktop environment"	off
 				LXQT "Light Desktop environment with QT"	off
-				Unity "Ubun0tu's Desktop environment"	off
+				Unity "Ubuntu's Desktop environment"	off
 				DDE "Deepin's Desktop environment"	off
 				OpenBox "Simple and minimalistic DE"	off
 				i3 "Tiled Window manager"	off
